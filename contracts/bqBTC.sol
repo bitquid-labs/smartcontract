63ab8ce9419aca7bc25d2fe8e964e212489e8334
tosynthegeek <oluwatosinojedapo@gmail.com>
2024-11-30 16:46:56 +0100

    feat: improved validation on mint function

--- contracts/bqBTC.sol
+++ contracts/bqBTC.sol
@@ -10,9 +10,16 @@ contract bqBTC is ERC20, Ownable {
     address public poolAddress;
     address public coverAddress;
     address public initialOwner;
-    IERC20 public bscBTC; // 0x6ce8da28e2f864420840cf74474eff5fd80e65b8
-    uint256 public minMintAmount;
+    mapping(uint256 chainId => uint256 multiplier) public networkMultipliers;
+    IERC20 public alternativeToken; // 0x6ce8da28e2f864420840cf74474eff5fd80e65b8
     address govContract;
+
+    event Mint(
+        address indexed account,
+        uint256 amount,
+        uint256 chainId,
+        bool native
+    );
 
     constructor(
         string memory name,
@@ -20,41 +27,58 @@ contract bqBTC is ERC20, Ownable {
         uint8 decimals_,
         uint256 initialSupply,
         address _initialOwner,
-        address bscbtc,
-        uint256 _minMintAmount
+        address _alternativeToken,
+        uint256 _multiplier
     ) ERC20(name, symbol) Ownable(_initialOwner) {
+        require(
+            _multiplier > 0 && _multiplier < 1100,
+            "Multiplier must be greater than zero"
+        );
+        require(_alternativeToken != address(0), "Invalid token address");
         _customDecimals = decimals_;
         _mint(msg.sender, initialSupply);
         initialOwner = _initialOwner;
-        bscBTC = IERC20(bscbtc);
-        minMintAmount = _minMintAmount;
+        alternativeToken = IERC20(_alternativeToken);
+        networkMultipliers[block.chainid] = _multiplier;
     }
 
     function decimals() public view virtual override returns (uint8) {
         return _customDecimals;
     }
 
-    function mint(
-        address account,
-        uint256 amount,
-        uint256 btcAmount
-    ) external payable {
-        bool nativeSent = msg.value >= minMintAmount;
+    function mint(address account, uint256 btcAmount) external payable {
+        uint256 currentChainId = block.chainid;
+        uint256 networkMultiplier = networkMultipliers[currentChainId];
+
+        require(networkMultiplier > 0, "No multiplier for this network");
+        require(account != address(0), "Invalid account address");
+
+        bool nativeSent = msg.value > 0;
         bool btcSent = false;
+        uint256 mintAmount;
+
+        if (nativeSent) {
+            mintAmount = (msg.value * networkMultiplier) / 1 ether;
+        }
 
         if (!nativeSent) {
+            mintAmount = (btcAmount * networkMultiplier) / 1 ether;
+            require(btcAmount > 0, "amount must be greater than 0");
             require(
-                btcAmount >= minMintAmount,
-                "amount must be greater than 0"
-            );
-            require(
-                bscBTC.transferFrom(msg.sender, address(this), btcAmount),
+                alternativeToken.transferFrom(
+                    msg.sender,
+                    address(this),
+                    btcAmount
+                ),
                 "Insufficient BTC tokens sent to mint"
             );
             btcSent = true;
         }
+
         require(nativeSent || btcSent, "Insufficient tokens sent to mint");
-        _mint(account, amount);
+        _mint(account, mintAmount);
+
+        emit Mint(account, mintAmount, currentChainId, nativeSent);
     }
 
     function burn(address account, uint256 amount) external {
@@ -118,6 +142,17 @@ contract bqBTC is ERC20, Ownable {
         coverAddress = cover;
         poolAddress = pool;
         govContract = gov;
+    }
+
+    function setNetworkMultiplier(
+        uint256 chainId,
+        uint256 multiplier
+    ) external onlyOwner {
+        require(
+            multiplier > 0 && multiplier < 1100,
+            "Multiplier must be greater than zero"
+        );
+        networkMultipliers[chainId] = multiplier;
     }
 
     modifier onlyBQContracts() {
